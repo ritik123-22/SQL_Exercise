2457cec8e923293394fe213c2e3cbe810cb793df
ritik123-22 <113696195+ritik123-22@users.noreply.github.com>
2024-05-14 02:37:30 +0530

    Update Impledge_QA_Ritik.sql

--- Impledge_QA_Ritik.sql
+++ Impledge_QA_Ritik.sql
@@ -1,8 +1,13 @@
+--TASK 1:--SELECT the details of Doctors(s) who has got Admissions
+
 Update [Admissions] Set attending_doctor_id = 29 where attending_doctor_id = 3;
 Update [Admissions] Set patient_id = 4 where patient_id = 35;
 SELECT DISTINCT Doctor_ID, first_Name
 FROM Doctors
 JOIN Admissions ON Doctors.Doctor_ID = Admissions.attending_Doctor_ID;
+
+
+--TASK 2:--SELECT the details of Doctors(s) for whom there is no Admissions.
 
 Update [Admissions] Set attending_doctor_id = 29 where attending_doctor_id = 3;
 Update [Admissions] Set patient_id = 4 where patient_id = 35;
@@ -10,6 +15,7 @@ SELECT Doctor_ID,first_Name
 FROM Doctors
 WHERE Doctor_ID NOT IN (SELECT attending_Doctor_ID FROM Admissions);
 
+--TASK 3:--SELECT the details of Patients(s) whose Admission can’t be completed due to missing doctor details
 
 Update [Admissions] Set attending_doctor_id = 29 where attending_doctor_id = 3;
 Update [Admissions] Set patient_id = 4 where patient_id = 35;
